c39ed57def0c58deeef66953f4bea0ddcdb35607
Judenilson Araujo <judenilson@hotmail.com>
2022-10-22 21:50:10 -0300

    Implementado conexao com app

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebSocketsServer.h>
 
 #define RX_PIN 26 //Pino 26 RX DO SENSOR!
 #define TX_PIN 25 //Pino 25 TX DO SENSOR!
@@ -13,13 +15,155 @@ Bem como a cada 416us temos 1 bit!
 A primeira msg, que é uma msg do sistema é de 1 byte. Ou seja, 8 amostras, 3.33ms. 
 */
 
-void setup() {
-  Serial.begin(115200);
+boolean send_log = false;
+WebSocketsServer webSocket = WebSocketsServer(81);
+int counter = 0;
+
+void configSerialMonitor(int num = 9600)
+{
+  Serial.begin(num);
   delay(1000);
+  Serial.println("Monitor working");
+}
+
+void connectToRouter(const char *ssid, const char *password)
+{
+  Serial.print("Conecting to router");
+
+  // https://www.youtube.com/watch?v=B9jJI7p2Gw4&ab_channel=MishMashLabs, setando ip fixo
+  // IPAddress local_IP(192, 168, 1, 199);
+  // IPAddress gateway(192, 168, 1, 1);
+  IPAddress local_IP(192, 168, 0, 199);
+  IPAddress gateway(192, 168, 0, 1);
+  IPAddress subnet(255, 255, 0, 0);
+  // if (!WiFi.config(local_IP, gateway, subnet))
+  //   Serial.println("STA Failed to configure");
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(" .");
+  }
+  Serial.println("\nConected");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void receiveMsg(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  { // switch on the type of information sent
+  // case WStype_ERROR:
+  //   break;
+  // case WStype_BIN:
+  //   break;
+  // case WStype_FRAGMENT_TEXT_START:
+  //   break;
+  // case WStype_FRAGMENT_BIN_START:
+  //   break;
+  // case WStype_FRAGMENT:
+  //   break;
+  // case WStype_FRAGMENT_FIN:
+  //   break;
+  case WStype_PING:
+    Serial.println("Client " + String(num) + " pinged");
+    break;
+  case WStype_PONG:
+    Serial.println("Client " + String(num) + " ponged");
+    break;
+  case WStype_DISCONNECTED: // if a client is disconnected, then type == WStype_DISCONNECTED
+    Serial.println("Client " + String(num) + " disconnected");
+    break;
+  case WStype_CONNECTED: // if a client is connected, then type == WStype_CONNECTED
+    Serial.println("Client " + String(num) + " connected");
+    Serial.println("Server has " + String(webSocket.connectedClients()) + " clients connected");
+    Serial.println("Server has " + webSocket.remoteIP(num).toString() + " clients connected");
+    // webSocket.enableHeartbeat();
+    // webSocket.enableHeartbeat(webSocket.connectedClients());
+    break;
+  case WStype_TEXT: // if a client has sent data, then type == WStype_TEXT
+    char payloadString[strlen((char *)(payload))];
+
+    strcpy(payloadString, (char *)(payload));
+    if (strcmp(payloadString, "start logs") == 0)
+      send_log = true;
+    else if (strcmp(payloadString, "stop logs") == 0)
+      send_log = false;
+    // connection confirmation
+    else if (strcmp(payloadString, "ping") == 0)
+      webSocket.sendTXT(num, "pong");
+    // get connected ports
+    else if (strcmp(payloadString, "ports") == 0)
+    {
+      // manda as portas que tem senor conectado
+      webSocket.sendTXT(num, "{\"connectedPorts\":[\"port1\",\"port2\"]}"); // caso de um sensor de toque e outro de ultrasónico
+      // webSocket.sendTXT(num, "{\"connectedPorts\":[]}"); // caso não tenha senosres conectados
+    }
+    else
+    {
+      // try to decipher the JSON string received
+      // DeserializationError error = deserializeJson(doc_received_payload, payloadString);
+      // if (error)
+      // {
+      //   Serial.print(F("deserializeJson() failed: "));
+      //   Serial.println(error.f_str());
+      //   break;
+      // }
+      // else
+      // {
+      //   // JSON string was received correctly, so information can be retrieved:
+      //   const char *cmd = doc_received_payload["cmd"];
+      //   Serial.println("Received json from wsClient: " + String(num));
+      //   Serial.println("cmd: " + String(cmd));
+      //   if (strcmp(cmd, "port config") == 0)
+      //   {
+      //     const char *portName = doc_received_payload["portName"];
+      //     const char *sensorType = doc_received_payload["sensorType"];
+      //     Serial.println("portName: " + String(portName));
+      //     Serial.println("sensorType: " + String(sensorType));
+      //   }
+      //   Serial.println("");
+      // }
+    }
+
+    Serial.printf("received: payload [%u]: %s\n", num, payloadString);
+    // try to decipher the JSON string received
+    // DeserializationError error = deserializeJson(doc_rx, payload);
+    // if (error) {
+    //   Serial.print(F("deserializeJson() failed: "));
+    //   Serial.println(error.f_str());
+    //   return;
+    // }
+    // else {
+    //   // JSON string was received correctly, so information can be retrieved:
+    //   const char* g_brand = doc_rx["brand"];
+    //   const char* g_type = doc_rx["type"];
+    //   const int g_year = doc_rx["year"];
+    //   const char* g_color = doc_rx["color"];
+    //   Serial.println("Received guitar info from user: " + String(num));
+    //   Serial.println("Brand: " + String(g_brand));
+    //   Serial.println("Type: " + String(g_type));
+    //   Serial.println("Year: " + String(g_year));
+    //   Serial.println("Color: " + String(g_color));
+    // }
+    // Serial.println("");
+    break;
+  };
+}
+
+void setup() {
+  configSerialMonitor(115200);
+  connectToRouter("COLOCAR SSID", "COLOCAR SENHA");  
+  delay(1000);
+
   pinMode(RX_PIN, INPUT);
   pinMode(TX_PIN, INPUT);  
   delay(1000);
-  Serial.println("Iniciando comunicacao serial"); 
+
+  webSocket.begin();
+  webSocket.onEvent(receiveMsg);
+  Serial.print("Configurado");
 }
 
 int estadoAltoRepouso(){
@@ -98,10 +242,33 @@ void lendoDados(){
   return;
 }
 
+void enviandoDados(){
+  webSocket.loop();
+  int bufferDelay = 20;
+  if (send_log && webSocket.connectedClients())
+  {
+    // webSocket.broadcastTXT(msg, strlen(msg));
+    // String logs = "{\"logs\":{\"port1\":[{\"value\":" + String(random(255)) + ",\"time\":\"hh:mm:ss:llll\"},{\"value\":" + String(random(255)) + ",\"time\":\"hh:mm:ss:llll\"},{\"value\":" + String(random(255)) + ",\"time\":\"hh:mm:ss:llll\"}],\"port2\":[{\"value\":" + String(random(255)) + ",\"time\":\"hh:mm:ss:llll\"},{\"value\":" + String(random(255)) + ",\"time\":\"hh:mm:ss:llll\"},{\"value\":" + String(random(255)) + ",\"time\":\"hh:mm:ss:llll\"}]}}";
+    String logs = "{\"logs\":{\"port1\":[";
+    for (int i = 0; i < bufferDelay; i++) {
+      if (i == bufferDelay - 1) logs.concat("{\"value\":" + String(valorDoSensor) + ",\"time\":\"hh:mm:ss:llll\"}");
+      else logs.concat("{\"value\":" + String(valorDoSensor) + ",\"time\":\"hh:mm:ss:llll\"},");
+    }
+    logs.concat("],\"port2\":[");
+    for (int i = 0; i < bufferDelay; i++) {
+      if (i == bufferDelay - 1) logs.concat("{\"value\":" + String(valorDoSensor) + ",\"time\":\"hh:mm:ss:llll\"}");
+      else logs.concat("{\"value\":" + String(valorDoSensor) + ",\"time\":\"hh:mm:ss:llll\"},");
+    }
+    logs.concat("]}}");
+    webSocket.broadcastTXT(logs);
+    counter++;
+    delay(bufferDelay);
+  }
+}
+
+
 void loop() {
-
   lendoDados();  
-
+  enviandoDados();
   Serial.println(valorDoSensor);
-
 }
