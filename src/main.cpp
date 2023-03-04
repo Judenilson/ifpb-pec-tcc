537e073281604eb002e0f42ee6c59adbe3643960
Antonio Albuqeurque <carlosalbuquerque359@gmail.com>
2023-03-04 18:28:46 -0300

    setting up static IP

--- src/main.cpp
+++ src/main.cpp
@@ -32,13 +32,14 @@ void connectToRouter(const char *ssid, const char *password)
   Serial.print("Conecting to router");
 
   // https://www.youtube.com/watch?v=B9jJI7p2Gw4&ab_channel=MishMashLabs, setando ip fixo
-  // IPAddress local_IP(192, 168, 1, 199);
-  // IPAddress gateway(192, 168, 1, 1);
-  IPAddress local_IP(192, 168, 0, 199);
-  IPAddress gateway(192, 168, 0, 1);
+  IPAddress local_IP(192, 168, 1, 199);
+  IPAddress gateway(192, 168, 1, 1);
   IPAddress subnet(255, 255, 0, 0);
-  // if (!WiFi.config(local_IP, gateway, subnet))
-  //   Serial.println("STA Failed to configure");
+  // IPAddress local_IP(192, 168, 0, 199);
+  // IPAddress gateway(192, 168, 0, 1);
+  // IPAddress subnet(255, 255, 0, 0);
+  if (!WiFi.config(local_IP, gateway, subnet))
+    Serial.println("STA Failed to configure");
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
